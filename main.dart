fe019b31e5d559e9e0a29fecac600f151451d84a
CyrianCrPro <161606497+CyrianCrPro@users.noreply.github.com>
2024-04-07 18:21:21 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Champions Dashboard',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: HomePage(),
+    );
+  }
+}
